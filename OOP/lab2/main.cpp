587e465126206a5f5de5477c891f49692acbb7cc
Karma <tutkarma@gmail.com>
2017-09-13 00:41:49 +0300

    add, delete and print list

---
+++ OOP/lab2/main.cpp
@@ -0,0 +1,41 @@
+#include <cstdlib>
+#include <iostream>
+#include "TList.h"
+
+int main()
+{
+    int act = 0;
+    //uint32_t kek = 0;
+    TList list;
+    Trapeze trapeze;
+    std::cout << "Choose an operation:" << std::endl;
+    std::cout << "1) Add trapeze" << std::endl;
+    std::cout << "2) Delete trapeze from list" << std::endl;
+    std::cout << "3) Print list"  << std::endl;
+    std::cout << "0) Exit" << std::endl;
+    while (std::cin >> act && act) {
+        switch(act) {
+        case 1:
+            std::cin >> trapeze;
+            list.Push(trapeze);
+            //kek = list.GetLength();
+            //std::cout << kek << std::endl;
+            break;
+        case 2:
+            trapeze = list.Pop();
+            break;
+        case 3:
+            std::cout << list << std::endl;
+            break;
+        default:
+            std::cout << "Incorrect command" << std::endl;;
+            break;
+        }
+        std::cout << "Choose an operation:" << std::endl;
+        std::cout << "1) Add trapeze" << std::endl;
+        std::cout << "2) Delete trapeze from list"  << std::endl;
+        std::cout << "3) Print list"  << std::endl;
+        std::cout << "0) Exit" << std::endl;
+    }
+    return 0;
+}
