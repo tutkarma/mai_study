90112145ace9c2c931afc2091343b6861b568687
Karma <tutkarma@gmail.com>
2017-09-27 14:23:10 +0300

    fix create dir and read file

--- OS/lab1/main.c
+++ OS/lab1/main.c
@@ -9,66 +9,74 @@
 #include <inttypes.h>
 #include <errno.h>
 
+#define BUFSI 256
+#define STRINGCUT 4
 
 int main(void)
 {
-    const int BUFSIZ = 256;
+    //const int BUFSIZ = 256;
     const char *testSting = "easyeasyeasy";
-    char buf[BUFSIZ];
-    char bufTrunc[BUFSIZ];
+    char buf[BUFSI];
+    char bufTrunc[BUFSI];
     int fd;
     ssize_t rd, wr;
 
-
     //create file "test"
-    fd = creat("test", S_IREAD | S_IWRITE);
+    fd = creat("test", O_RDWR); //S_IREAD | S_IWRITE
     if (fd == -1) {
-        perror("Error in opening test\n");
+        perror("Error opening test");
     } else {
         fprintf(stdout, "test opened for read/write access\n");
         fprintf(stdout, "test opened is empty\n");
     }
 
     if (fsync(fd) == -1) { //синхронизирует состояние файла в памяти с его состоянием на диске
-        perror("Error: fsync\n");
+        perror("Error fsync");
     }
 
     // write test string in file "test"
     if (wr = write(fd, testSting, strlen(testSting)) == -1) { /* записывает количество байтов в 3 аргументе из буфера в файл с дискриптором fd,
                                                                   возвращает количество записанных байтов или -1 в случае ошибки */
-        perror("Error: write\n");
+        perror("Error writing");
     }
-    lseek(fd, 0L, 0); // вернуться к началу файла
+    if (lseek(fd, 0, SEEK_SET) == -1) { // вернуться к началу файла
+        perror("Error lseek");
+    }
+
+    if (close(fd) == -1) {
+        perror("Error closing");
+    }
 
     // read from file in buffer
-    if (read(fd, buf, sizeof(testSting)) == sizeof(testSting)) {
+    fd = open("test", O_RDWR);
+    if (read(fd, buf, strlen(testSting)) == strlen(testSting)) {
         fprintf(stdout, "%s", buf);
+        fprintf(stdout, " was written to test\n");
     } else {
-        perror("Error reading test\n");
+        perror("Error reading"); 
     }
 
-    //close file
     if (close(fd) == -1) {
-        perror("Error: close\n");
+        perror("Error closing");
     }
-    if (truncate("test", 2) == -1) { // укорачивает файл до указанной длины, отсеченные данные теряются
-        perror("Error: truncate\n");
+    if (truncate("test", STRINGCUT) == -1) { // укорачивает файл до указанной длины, отсеченные данные теряются
+        perror("Error truncate");
     }
 
     // open file for reading
     fd = open("test", O_RDONLY);
-    rd = read(fd, bufTrunc, BUFSIZ);
+    rd = read(fd, bufTrunc, BUFSI);
     if (rd == -1) {
         if (errno == EINTR) { // системный вызов прерван сигналом
             fprintf(stdout, "EINTR\n");
         }
-        perror("Error read\n");
+        perror("Error reading");
     }
 
-    fprintf(stdout, "String after trunc\n");
-    fprintf(stdout, "%s", bufTrunc);
+    fprintf(stdout, "String after trunc: ");
+    fprintf(stdout, "%s\n", bufTrunc);
     if (close(fd) == -1) {
-        perror("Error close\n");
+        perror("Error closing");
     }
 
 
@@ -76,8 +84,8 @@ int main(void)
     if (fork() !=  0) {
         wait((int *) 0); // нулевой указатель -- возвращаемое значение не сохранено
     } else {
-        execl("./mkdir", "mkdir", "newdir", (char *) NULL); // передаем аргументы в командную строку (когда их количество известно)
-        perror("Error exec\n");
+        execl("/bin/mkdir", "mkdir", "newdir", (char *) NULL); // передаем аргументы в командную строку (когда их количество известно)
+        perror("Error exec");
     }
 
     // use new directory
@@ -88,7 +96,7 @@ int main(void)
                                                                          (0664 аналогично S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH 
                                                                          -- доступен в режиме чтение-запсь для пользователя и группы
                                                                          и только чтения для остальных пользователей) */
-        perror("Error open\n");
+        perror("Error opening");
     }
     write(fd, "Hello, world\n", 14);
     close(fd);
@@ -97,7 +105,7 @@ int main(void)
     if (pid == 0) {
         fprintf(stdout, "It's child process, pid = ");
         fprintf(stdout, "%ld\n", (intmax_t)getpid());
-        fprintf(stdout, "Parent's pid = \n");
+        fprintf(stdout, "Parent's pid = ");
         fprintf(stdout, "%ld\n", (intmax_t)getppid());
     } else if (pid > 0) {
         fprintf(stdout, "It's parent process, pid = ");
@@ -106,7 +114,7 @@ int main(void)
         fprintf(stdout, "%ld\n", (intmax_t)getppid());
         wait(NULL);
     } else if (pid == -1) {
-        perror("Error: fork.\n");
+        perror("Error fork");
     }
 
 
