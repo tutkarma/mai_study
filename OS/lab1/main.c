2b04b85321261b3183b5cc18596ba02122130ec6
Karma <tutkarma@gmail.com>
2017-10-01 18:42:27 +0300

    remove trash

--- OS/lab1/main.c
+++ OS/lab1/main.c
@@ -1,6 +1,6 @@
 #include <sys/wait.h>
 #include <sys/types.h>
-#include <sys/stat.h> // S_IREAD, S_IWRITE
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,42 +9,40 @@
 #include <inttypes.h>
 #include <errno.h>
 
-#define BUFSI 256
+#define BUFSIZE 256
 #define STRINGCUT 4
 
 int main(void)
 {
-    //const int BUFSIZ = 256;
     const char *testSting = "easyeasyeasy";
-    char buf[BUFSI];
-    char bufTrunc[BUFSI];
+    char buf[BUFSIZE];
+    char bufTrunc[BUFSIZE];
     int fd;
     ssize_t rd, wr;
 
     //create file "test"
     fd = creat("test", O_RDWR); //S_IREAD | S_IWRITE
     if (fd == -1) {
-        perror("Error opening test");
+        perror("Error creat");
     } else {
         fprintf(stdout, "test opened for read/write access\n");
         fprintf(stdout, "test opened is empty\n");
     }
 
-    if (fsync(fd) == -1) { //синхронизирует состояние файла в памяти с его состоянием на диске
+    if (fsync(fd) == -1) {
         perror("Error fsync");
     }
 
     // write test string in file "test"
-    if (wr = write(fd, testSting, strlen(testSting)) == -1) { /* записывает количество байтов в 3 аргументе из буфера в файл с дискриптором fd,
-                                                                  возвращает количество записанных байтов или -1 в случае ошибки */
-        perror("Error writing");
+    if (wr = write(fd, testSting, strlen(testSting)) == -1) {
+        perror("Error write");
     }
-    if (lseek(fd, 0, SEEK_SET) == -1) { // вернуться к началу файла
+    if (lseek(fd, 0, SEEK_SET) == -1) {
         perror("Error lseek");
     }
 
     if (close(fd) == -1) {
-        perror("Error closing");
+        perror("Error close");
     }
 
     // read from file in buffer
@@ -53,52 +51,49 @@ int main(void)
         fprintf(stdout, "%s", buf);
         fprintf(stdout, " was written to test\n");
     } else {
-        perror("Error reading"); 
+        perror("Error read"); 
     }
 
     if (close(fd) == -1) {
-        perror("Error closing");
+        perror("Error close");
     }
-    if (truncate("test", STRINGCUT) == -1) { // укорачивает файл до указанной длины, отсеченные данные теряются
+    if (truncate("test", STRINGCUT) == -1) {
         perror("Error truncate");
     }
 
     // open file for reading
     fd = open("test", O_RDONLY);
-    rd = read(fd, bufTrunc, BUFSI);
-    if (rd == -1) {
-        if (errno == EINTR) { // системный вызов прерван сигналом
-            fprintf(stdout, "EINTR\n");
-        }
-        perror("Error reading");
+    rd = read(fd, bufTrunc, BUFSIZE);
+    if (rd == -1 && errno == EINTR) {
+        perror("Error read");
     }
 
     fprintf(stdout, "String after trunc: ");
     fprintf(stdout, "%s\n", bufTrunc);
     if (close(fd) == -1) {
-        perror("Error closing");
+        perror("Error close");
     }
 
 
     //create new directory, called newdir, using fork() and exec()
     if (fork() !=  0) {
-        wait((int *) 0); // нулевой указатель -- возвращаемое значение не сохранено
+        wait(NULL);
     } else {
-        execl("/bin/mkdir", "mkdir", "newdir", (char *) NULL); // передаем аргументы в командную строку (когда их количество известно)
+        execl("/bin/mkdir", "mkdir", "newdir", (char *) NULL);
         perror("Error exec");
     }
 
     // use new directory
-    if ((fd = open("newdir/newfile", O_RDWR | O_CREAT, 0644)) == -1) { /* первый аргумент -- путь до файла, 
-                                                                         второй -- флаги открытия (здесь открыть файл в режиме чтения и записи,
-                                                                         и автоматически создать файл, если такого не существует),
-                                                                         третий -- если создается новый файл, то задать права доступа
+    const char *msg = "I'm alive...\n";
+    if ((fd = open("newdir/newfile", O_RDWR | O_CREAT, 0644)) == -1) { /* открыть файл в режиме чтения и записи,
+                                                                         и автоматически создать файл, если такого не существует,
+                                                                         если создается новый файл, то задать права доступа
                                                                          (0664 аналогично S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH 
                                                                          -- доступен в режиме чтение-запсь для пользователя и группы
                                                                          и только чтения для остальных пользователей) */
         perror("Error opening");
     }
-    write(fd, "Hello, world\n", 14);
+    write(fd, msg, strlen(msg));
     close(fd);
 
     pid_t pid = fork();
@@ -116,7 +111,5 @@ int main(void)
     } else if (pid == -1) {
         perror("Error fork");
     }
-
-
     return 0;
 }
