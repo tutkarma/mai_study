3c9443f87b08b7814469a807808218d806ed40ea
Karma <tutkarma@gmail.com>
2017-10-08 23:38:56 +0300

    recursive fibonacci with pipe and fork

--- OS/lab2/main.c
+++ OS/lab2/main.c
@@ -6,57 +6,72 @@
 #include <unistd.h>
 #include <errno.h>
 
-//function to return exit code for PID
-static int ExitCode(pid_t pid)
-{
-    pid_t retpid;
-    int status;
 
-    retpid = waitpid(pid, &status, 0);
-    if (pid != retpid) {
-        fprintf(stderr, "Error waitpid\n");
-    }
+//Parent: reads from P1_READ, writes on P1_WRITE
+//Child: reads from P2_READ, writes on P2_WRITE
+#define P1_READ    0
+#define P2_WRITE   1
+#define P2_READ    2
+#define P1_WRITE   3
 
-    return WEXITSTATUS(status);
-}
+int fd[4];
 
-int Fib(size_t n)
+int Fib(int n)
 {
     pid_t pid1, pid2;
-    int status1, status2;
+    int buf1, buf2, res, status, bufread1, bufread2;
 
-    switch(n) {
-        case 0: return 0;
-        case 1: return 1;
-        case 2: return 1;
-        default: break;
+    if (n == 0) {
+        return 0;
+    } else if (n == 1 || n == 2) {
+        return 1;
     }
 
     pid1 = fork();
     if (pid1 == 0) { // child process 1 (fib(n-1))
-        exit(Fib(n - 1));
+        buf1 = Fib(n - 1);
+        close(fd[P1_READ]);
+        write(fd[P2_WRITE], &buf1, sizeof(buf1));
+        exit(0);
     } else if (pid1 < 0) {
         perror("fork");
+    } else if (pid1 > 0) {
+        pid2 = fork();
+        if (pid2 == 0) { // child process 2 (fib(n-2))
+            buf2 = Fib(n - 2);
+            close(fd[P1_READ]);
+            write(fd[P2_WRITE], &buf2, sizeof(buf2));
+            exit(0);
+        } else if (pid2 < 0) {
+            perror("fork");
+        }
     }
 
-    pid2 = fork();
-    if (pid2 == 0) { // child process 2 (fib(n-2))
-        exit(Fib(n - 2));
-    } else if(pid2 < 0) {
-        perror("fork");
+    if (waitpid(pid1, &status, 0) == -1) {
+        perror("waitpid");
+    }
+    if (waitpid(pid2, &status, 0) == -1) {
+        perror("waitpid");
     }
 
-    //get value from child process 1
-    status1 = ExitCode(pid1);
-    //get value from child process 2
-    status2 = ExitCode(pid2);
+    close(fd[P2_READ]);
+    read(fd[P1_READ], &bufread1, sizeof(bufread1));
+    read(fd[P1_READ], &bufread2, sizeof(bufread2));
+    res = bufread1 + bufread2;
+    close(fd[P1_READ]);
+    write(fd[P1_WRITE], &res, sizeof(res));
 
-    return status1 + status2;
+    return res;
 }
 
 int main(void)
 {
     int n = 0;
+
+    if (pipe(fd) == -1) {
+        perror("pipe");
+    }
+
     printf("Enter a sequence number: ");
     scanf("%d", &n);
     if (n < 0) {
@@ -64,5 +79,6 @@ int main(void)
     } else {
         printf("%d\n", Fib(n));
     }
+
     return 0;
 }
