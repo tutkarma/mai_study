bdd59a2981fae7d783eab1e8fa443d4493b81f9b
tutkarma <tutkarma@gmail.com>
2019-03-03 18:53:46 +0300

    OM: add quickest descent

--- OM/main.py
+++ OM/main.py
@@ -3,16 +3,20 @@ from math import sqrt
 
 def get_info(method, coefs, x0, h, eps):
     print(method)
-    print("f(x) = {0}x1^2 - x1x2 + {1}x2^2 + {2}x1 + {3}x2 + 13".format(
-        coefs[2], coefs[3], coefs[0], coefs[1]))
+    print("f(x) = {0}x1^2 - x1x2 + {1}x2^2 + {2}x1 + {3}x2 + {4}".format(
+        coefs[2], coefs[3], coefs[0], coefs[1], coefs[4]))
     print("x_0 = ({0} {1})^T".format(x0[0], x0[1]))
     print("h = {0}".format(h))
     print("eps = {0}".format(eps))
 
 
+def f(x, coefs):
+    return coefs[2] * x[0]**2 - x[0] * x[1] + coefs[3] * x[1]**2 + coefs[0] * x[0] + coefs[1] * x[1] + 13
+
+
 def grad(x, coefs):
-    dx1 = coefs[2] * 2 * x[0] + x[1] + coefs[0]
-    dx2 = coefs[3] * 2 * x[1] + x[0] + coefs[1]
+    dx1 = coefs[2] * 2 * x[0] - x[1] + coefs[0]
+    dx2 = coefs[3] * 2 * x[1] - x[0] + coefs[1]
     return [dx1, dx2]
 
 
@@ -21,18 +25,25 @@ def grad_val(x, coefs):
     return sqrt(dx[0] ** 2 + dx[1] ** 2)
 
 
-def f(x, coefs):
-    return coefs[2] * x[0]**2 - x[0] * x[1] + coefs[3] * x[1]**2 + coefs[0] * x[0] + coefs[1] * x[1] + 13
-
-
 def finish_iter_proccess(x, coefs, eps):
     print("Сhecking the condition of the end iterative process:")
-    sign = "<" if abs(grad_val(x, coefs)) < eps else ">="
+    sign = "<" if grad_val(x, coefs) < eps else ">="
     print("{0} {1} {2}". format(grad_val(x, coefs), sign, eps))
-    return abs(grad_val(x, coefs)) < eps
+    return grad_val(x, coefs) < eps
 
 
-def classical_method(coefs, x_0, h, eps):
+def get_step(x, coefs):
+    gr = grad(x, coefs)
+
+    coef1 = -2 * coefs[2] * x[0] * gr[0] + x[0] * gr[1] + x[1] * gr[0] - \
+            2 * coefs[3] * x[1] * gr[1] - coefs[0] * gr[0] - coefs[1] * gr[1] # h
+
+    coef2 = coefs[2] * gr[0] ** 2 - gr[0] * gr[1] + coefs[3] * gr[1] ** 2 # h^2
+
+    return (-coef1) / (coef2 * 2)
+
+
+def classical_method(coefs, x_prev, h, eps):
     get_info("Calssical method", coefs, x_prev, h, eps)
 
 
@@ -43,6 +54,7 @@ def gradient_descent(coefs, x_prev, h, eps):
     while not finish_iter_proccess(x_prev, coefs, eps):
         print("---------")
         print("Iteration #{}".format(k))
+        print("grad(f(x^{0})) = {1}". format(k, grad(x_prev, coefs)))
 
         gr = grad(x_prev, coefs)
         print("x = {0} - {1} * {2}".format(x_prev, h, gr))
@@ -65,8 +77,23 @@ def gradient_descent(coefs, x_prev, h, eps):
         x_prev = x
 
 
-def quickest_descent(coefs, x_0, h, eps):
+def quickest_descent(coefs, x_prev, h, eps):
     get_info("Quickest descent method", coefs, x_prev, h, eps)
+
+    k = 0
+    while not finish_iter_proccess(x_prev, coefs, eps):
+        print("---------")
+        print("Iteration #{}".format(k))
+        gr = grad(x_prev, coefs)
+        print("grad(f(x^({0}))) = {1}". format(k, gr))
+
+        h = get_step(x_prev, coefs)
+        print("h^({0}) = {1}".format(k, h))
+        x = [i - h * j for i, j in zip(x_prev, gr)]
+        print("x^({0}) = {1}".format(k, x))
+        x_prev = x
+        k += 1
+
 
 
 if __name__ == '__main__':
@@ -78,10 +105,10 @@ if __name__ == '__main__':
     x0 = list(map(int, input("Vector x_0 (two values) = ")))
     h = int(input("h = "))
     eps = int(input("eps = "))'''
-    coefs = [14, -7, 7, 7] # x1, x2, x1^2, x2^2
+    coefs = [14, -7, 7, 7, 13] # x1, x2, x1^2, x2^2, свободный коэф
     x_0 = [2, 3]
     h = 0.1
     eps = 0.9
     #classical_method(coefs, x_0, h, eps)
     gradient_descent(coefs, x_0, h, eps)
-    #quickest_descent(coefs, x_0, h, eps)
+    quickest_descent(coefs, x_0, h, eps)
