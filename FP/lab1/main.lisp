8cca1fcbc2189516ba7a3bbfa88417e6f40a60d6
tutkarma <tutkarma@gmail.com>
2019-03-16 10:44:19 +0300

    FP: lab1 change return value and add tests

--- FP/lab1/main.lisp
+++ FP/lab1/main.lisp
@@ -1,9 +1,4 @@
 (defun castle-moves (k l m n)
   (cond ((or (= k m)
-              (= l n)) "T")
+              (= l n)) t)
         (t (values k n))))
-
-(castle-moves 1 1 1 1)
-(castle-moves 4 5 7 8)
-(castle-moves 7 7 2 4)
-(castle-moves 2 6 6 6)
