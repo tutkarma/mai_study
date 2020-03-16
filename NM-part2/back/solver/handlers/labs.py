ff3d310c4969b8c61784345b3e2e4b94b35e0035
tutkarma <tutkarma@gmail.com>
2020-03-16 21:40:04 +0300

    NM part 2: add back lab6

--- NM-part2/back/solver/handlers/labs.py
+++ NM-part2/back/solver/handlers/labs.py
@@ -2,10 +2,11 @@ import json
 
 import numpy as np
 from werkzeug.exceptions import BadRequest
-from flask import Blueprint, request, Response, jsonify
+from flask import Blueprint, request, Response
 from flask_cors import CORS
 
 from solver.labs.lab5 import ParabolicSolver
+from solver.labs.lab6 import HyperbolicSolver
 
 
 labs = Blueprint('labs', __name__)
@@ -29,7 +30,6 @@ def get_solution(lab_id):
     elif lab_id ==8:
         resp = solve_lab8(data)
 
-
     return Response(
         json.dumps(resp),
         status=200
@@ -39,6 +39,7 @@ def get_solution(lab_id):
 def solve_lab5(data):
     equation_type = data['equation_type']
     N, K, T = int(data['N']), int(data['K']), int(data['T'])
+
     params = {
         'a': 1,
         'b': 0,
@@ -56,7 +57,6 @@ def solve_lab5(data):
         'bound_type': 'a1p2',
     }
 
-
     p1d7 = ParabolicSolver(params, equation_type)
     resp = {
         'numerical': p1d7.solve(N, K, T).tolist(),
@@ -67,8 +67,38 @@ def solve_lab5(data):
 
 
 def solve_lab6(data):
-    pass
+    equation_type = data['equation_type']
+    N, K, T = int(data['N']), int(data['K']), int(data['T'])
 
+    params = {
+        'a': 1,
+        'b': 2,
+        'c': -3,
+        'd': 2,
+        'l': np.pi / 2,
+        'f': lambda: 0,
+        'alpha': 1,
+        'beta': 0,
+        'gamma': 1,
+        'delta': 0,
+        'psi1': lambda x: np.exp(-x) * np.cos(x),
+        'psi2': lambda x: -np.exp(-x) * np.cos(x),
+        'psi1_dir1': lambda x: -np.exp(-x) * np.sin(x) - np.exp(-x) * np.cos(x),
+        'psi1_dir2': lambda x:  2 * np.exp(-x) * np.sin(x),
+        'phi0': lambda t: np.exp(-t) * np.cos(2 * t),
+        'phil': lambda x: 0,
+        'bound_type': 'a1p2',
+        'approximation': 'p1',
+        'solution': lambda x, t: np.exp(-t - x) * np.sin(x) * np.sin(2 * t),
+    }
+
+    p2d7 = HyperbolicSolver(params, equation_type)
+    resp = {
+        'numerical': p2d7.solve(N, K, T).tolist(),
+        'analytic': p2d7.solve_analytic(N, K, T).tolist()
+    }
+
+    return resp
 
 def solve_lab7(data):
     pass
---
+++ NM-part2/back/solver/labs/lab6.py
@@ -0,0 +1,196 @@
+import numpy as np
+
+from solver.labs.utils import tma
+
+
+class EquationData:
+    def __init__(self, params):
+        self.a = params['a']
+        self.b = params['b']
+        self.c = params['c']
+        self.d = params['d']
+        self.l = params['l']
+        self.f = params['f']
+        self.alpha = params['alpha']
+        self.beta = params['beta']
+        self.gamma = params['gamma']
+        self.delta = params['delta']
+        self.psi1 = params['psi1']
+        self.psi2 = params['psi2']
+        self.psi1_dir1 = params['psi1_dir1']
+        self.psi1_dir2 = params['psi1_dir2']
+        self.phi0 = params['phi0']
+        self.phil = params['phil']
+        self.bound_type = params['bound_type']
+        self.approximation = params['approximation']
+        self.solution = params['solution']
+
+
+class HyperbolicSolver:
+    def __init__(self, params, equation_type):
+        self.data = EquationData(params)
+        try:
+            self.solve_func = getattr(self, f'_{equation_type}_solve')
+        except:
+            raise Exception("This type does not exist")
+
+    def solve(self, N, K, T):
+        self.h = self.data.l / N;
+        self.tau = T / K;
+        self.sigma = self.tau ** 2 / (self.h ** 2)
+        return self.solve_func(N, K, T)
+
+    def solve_analytic(self, N, K, T):
+        self.h = self.data.l / N;
+        self.tau = T / K;
+        self.sigma = self.tau ** 2 / (self.h ** 2)
+        u = np.zeros((K, N))
+        for k in range(K):
+            for j in range(N):
+                u[k][j] = self.data.solution(j * self.h, k * self.tau)
+        return u
+
+    def _implicit_solve(self, N, K, T):
+        u = np.zeros((K, N))
+
+        for i in range(0, N):
+            u[0][i] = self.data.psi1(i * self.h)
+
+        for i in range(0, N):
+            x = i + self.h
+            if self.data.approximation == 'p1':
+                u[1][i] = self.data.psi1(x) + self.data.psi2(x) * self.tau + self.data.psi1_dir2(x) * self.tau ** 2 / 2
+            elif self.data.approximation == 'p2':
+                k = self.tau ** 2 / 2
+                u[1][i] = (1 + self.data.c * k) * self.data.psi2(x) + \
+                self.data.a * k * self.data.psi1_dir2(x) + \
+                self.data.b * k * self.data.psi1_dir1(x) + \
+                (self.tau - self.data.d * k) * self.data.psi1(x) + \
+                k * self.data.f()
+
+        a = np.zeros(N)
+        b = np.zeros(N)
+        c = np.zeros(N)
+        d = np.zeros(N)
+
+        for k in range(2, K):
+            for j in range(1, N - 1):
+                a[j] = self.sigma
+                b[j] = -(1 + 2 * self.sigma)
+                c[j] = self.sigma
+                d[j] = -2 * u[k - 1][j] + u[k - 2][j]
+
+            if self.data.bound_type == 'a1p2':
+                b[0] = self.data.alpha / self.h / (self.data.beta - self.data.alpha / self.h)
+                c[0] = 1
+                d[0] = 1 / (self.data.beta - self.data.alpha / self.h) * self.data.phi0(k * self.tau)
+                a[-1] = -self.data.gamma / self.h / (self.data.delta + self.data.gamma / self.h)
+                d[-1] = 1 / (self.data.delta + self.data.gamma / self.h) * self.data.phil(k * self.tau)
+            elif self.data.bound_type == 'a2p3':
+                k1 = 2 * self.h * self.data.beta - 3 * self.data.alpha
+                k2 = 2 * self.h * self.data.delt + 3 * self.data.gamma
+                omega = self.tau ** 2 * self.data.b / (2 * self.h)
+                xi = self.data.d * self.tau / 2
+
+                b[0] = 4 * self.data.alpha - self.data.alpha / (self.sigma + omega) * (1 + xi + 2 * self.sigma - self.data.c * self.tau ** 2)
+                c[0] = k1 - self.data.alpha * (omega - self.sigma) / (omega + self.sigma)
+                d[0] = 2 * self.h * self.data.phi0(k * self.tau) + self.data.alpha * d[1] / (-sigma - omega)
+                a[-1] = -self.data.gamma / (omega - sel.sigma) * (1 + xi + 2 * self.sigma - self.data.c * self.tau ** 2) - 4 * self.data.gamma
+                d[-1] = 2 * self.h * self.data.phil(k * self.tau) - self.data.gamma * d[-2] / (omega - self.sigma)
+            elif self.data.bound_type == 'a2p2':
+                b[0] = 2 * self.data.a / self.h
+                c[0] = -2 * self.data.a / self.h + self.h / self.tau ** 2 - self.data.c * self.h + \
+                    -self.data.d * self.h / (2 * self.tau) + \
+                    self.data.beta / self.data.alpha * (2 * self.data.a + self.data.b * self.h)
+                d[0] = self.h / self.tau ** 2 * (u[k - 2][0] - 2 * u[k - 1][0]) - self.h * self.data.f() + \
+                    -self.data.d * self.h / (2 * self.tau) * u[k - 2][0] + \
+                    (2 * self.data.a - self.data.b * self.h) / self.data.alpha * self.data.phi0(k * self.tau)
+
+                a[-1] =-b[0]
+
+                d[-1] = self.h / self.tau ** 2 * (-u[k - 2][0] + 2 * u[k - 1][0]) + self.h * self.data.f() + \
+                    seld.data.d * self.h / (2 * self.tau) * u[k - 2][0] + \
+                    (2 * self.data.a + self.data.b * self.h) / self.data.alpha * self.data.phil(k * self.tau)
+
+            u[k] = tma(a, b, c, d)
+
+        return u
+
+    def _left_bound_a1p2(self, u, k, t):
+        return -(self.data.alpha / self.h) / (self.data.beta - self.data.alpha / self.h) * u[k - 1][1] \
+               + self.data.phi0(t) / (self.data.beta - self.data.alpha / self.h)
+
+    def _right_bound_a1p2(self, u, k, t):
+        return (self.data.gamma / self.h) / (self.data.delta + self.data.gamma / self.h) * u[k - 1][-2] \
+               + self.data.phil(t) / (self.data.delta + self.data.gamma / self.h)
+
+    def _left_bound_a2p3(self, u, k, t):
+        denom = 2 * self.h * self.data.beta - 3 * self.data.alpha
+        return self.data.alpha / denom * u[k - 1][2] - 4 * self.data.alpha / denom * u[k - 1][1] \
+               + 2 * self.h / denom * self.data.phi0(t)
+
+    def _right_bound_a2p3(self, u, k, t):
+        denom = 2 * self.h * self.data.delta + 3 * self.data.gamma
+        return 4 * self.data.gamma / denom * u[k - 1][-2] - self.data.gamma / denom * u[k - 1][-3] \
+               + 2 * self.h / denom * self.data.phil(t)
+
+    def _left_bound_a2p2(self, u, k, t):
+        n = self.data.c * self.h - 2 * self.data.a / self.h - self.h / self.tau ** 2 - self.data.d * self.h / (2 * self.tau) + \
+            self.data.beta / self.data.alpha * (2 * self.data.a - self.data.b * self.h)
+        return 1 / n * (- 2 * self.data.a / self.h * u[k][1] +
+                        self.h / self.tau ** 2 * (u[k - 2][0] - 2 * u[k - 1][0]) +
+                        -self.data.d * self.h / (2 * self.tau) * u[k - 2][0] +
+                        -self.h * self.data.f() +
+                        (2 * self.data.a - self.data.b * self.h) / self.data.alpha * self.data.phi0(t))
+
+    def _right_bound_a2p2(self, u, k, t):
+        n = -self.data.c * self.h + 2 * self.data.a / self.h + self.h / self.tau ** 2 + self.data.d * self.h / (2 * self.tau) + \
+            self.data.delta / self.data.gamma * (2 * self.data.a + self.data.b * self.h)
+        return 1 / n * (2 * self.data.a / self.h * u[k][-2] +
+                        self.h / self.tau ** 2 * (2 * u[k - 1][-1] - u[k - 2][-1]) +
+                        self.data.d * self.h / (2 * self.tau) * u[k - 2][-1] +
+                        self.h * self.data.f() +
+                        (2 * self.data.a + self.data.b * self.h) / self.data.gamma * self.data.phil(t))
+
+    def _explicit_solve(self, N, K, T):
+        u = np.zeros((K, N))
+
+        for i in range(0, N):
+            u[0][i] = self.data.psi1(i * self.h)
+
+        for i in range(0, N):
+            x = i + self.h
+            if self.data.approximation == 'p1':
+                u[1][i] = self.data.psi1(x) + self.data.psi2(x) * self.tau + self.data.psi1_dir2(x) * self.tau ** 2 / 2
+            elif self.data.approximation == 'p2':
+                k = self.tau ** 2 / 2
+                u[1][i] = (1 + self.data.c * k) * self.data.psi2(x) + \
+                self.data.a * k * self.data.psi1_dir2(x) + \
+                self.data.b * k * self.data.psi1_dir1(x) + \
+                (self.tau - self.data.d * k) * self.data.psi1(x) + \
+                k * self.data.f()
+
+        if self.data.bound_type == 'a1p2':
+            left_bound = self._left_bound_a1p2
+            right_bound = self._right_bound_a1p2
+        elif self.data.bound_type == 'a2p3':
+            left_bound = self._left_bound_a2p3
+            right_bound = self._right_bound_a2p3
+        elif self.data.bound_type == 'a2p2':
+            left_bound = self._left_bound_a2p2
+            right_bound = self._right_bound_a2p2
+
+        omega = self.tau ** 2 * self.data.b / (2 * self.h)
+        xi = self.data.d * self.tau / 2
+        c = 1 / (1 + xi)
+        for k in range(2, K):
+            t = k * self.tau
+            for j in range(1, N - 1):
+                u[k][j] = c * ((xi - 1) * u[k - 2][j] +
+                         (self.sigma - omega) * u[k - 1][j - 1] +
+                         (self.data.c * self.tau ** 2 - 2 * self.sigma + 2) * u[k - 1][j] +
+                         (self.sigma + omega) * u[k - 1][j + 1] +
+                         self.tau ** 2 * self.data.f())
+                u[k][0] = left_bound(u, k, t)
+                u[k][-1] = right_bound(u, k, t)
+        return u
